90968b5c74042a76860fbf5453a78979b0a20b7b
Eric Voong <evoong@go.pasadena.edu>
2026-01-07 11:12:49 -0800

    Update Covid PortfolioProject.sql

--- Covid PortfolioProject.sql
+++ Covid PortfolioProject.sql
@@ -1,6 +1,6 @@
 /*
-Covid 19 Data Exploration 
 
+Covid 19 Data Exploration 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 */
@@ -255,3 +255,4 @@ JOIN covidvaccinations vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 WHERE dea.continent IS NOT NULL;
+
